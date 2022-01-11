44a9c7407f1e2e589371879dfd36891c62e3c4dc
xRumi <mehedihasanrumi@yahoo.com>
2022-01-11 19:51:06 +0600

    webhook

---
+++ webhook.js
@@ -0,0 +1,16 @@
+const secret = "i_hate_FoF22";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+        if (req.headers['x-hub-signature'] == sig) {
+            console.log('webhook received, pulling latest changes');
+            exec('git pull');
+        }
+    });
+    res.end();
+}).listen(81);
