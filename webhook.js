3cb8b2a0efafbcb2a89f7f058be3095d4ce2bca1
xRumi <mehedihasanrumi@yahoo.com>
2022-01-11 20:03:48 +0600

    better webhook handler

--- webhook.js
+++ webhook.js
@@ -1,15 +1,22 @@
-const secret = "i_hate_FoF22";
-
-const http = require('http');
-const crypto = require('crypto');
-const exec = require('child_process').exec;
+const secret = "i_hate_FoF22",
+    http = require('http'),
+    crypto = require('crypto'),
+    spawn = require('child_process').spawn;
 
 http.createServer(function (req, res) {
     req.on('data', function(chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
         if (req.headers['x-hub-signature'] == sig) {
-            console.log('webhook received');
-            exec('git pull');
+            let pull = spawn('git', 'pull');
+            pull.stdout.on('data', function (data) {
+                console.log(data.toString());
+            });
+            pull.stderr.on('data', function (data) {
+                console.log(data.toString());
+            });
+            pull.on('exit', function (code) {
+                console.log('child process exited with code ' + code.toString());
+            });
         }
     });
     res.end();
