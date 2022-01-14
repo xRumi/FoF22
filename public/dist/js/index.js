3857e978a080641ab3639a23801011c0e1859eb4
xRumi <mehedihasanrumi@yahoo.com>
2022-01-14 19:02:19 +0600

    active nav

--- public/dist/js/index.js
+++ public/dist/js/index.js
@@ -13,6 +13,10 @@ function pre_render (url, title) {
 }
 
 function after_render (title) {
+    if (!$(`.${current_page}`).hasClass('active-link')) {
+        $(".active-link").removeClass('active-link');
+        $(`.${current_page}`).addClass('active-link');
+    }
     $('.nav__menu').css('transform', 'unset');
     $('.loader__center').fadeOut(100);
     document.title = title;
