870a8f9a3fcd9c232663f4c7e342a762da24f457
Mehedi Hasan Rumi <80669909+xRumi@users.noreply.github.com>
2022-03-02 20:19:18 +0600

    Bug fix?!

--- models/_User.js
+++ models/_User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("_user", new mongoose.Schema({
 
     verified: { type: Boolean, default: false },
-    email: { type: String, unique: true },
+    email: { type: String },
 
     created_at: { type: Date, default: Date.now }
 
