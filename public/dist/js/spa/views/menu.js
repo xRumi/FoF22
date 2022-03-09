88ed38923b32bca702848757a41a839395d8a826
xRumi <mehedihasanrumi@yahoo.com>
2022-03-09 12:15:42 +0600

    aaa

--- public/dist/js/spa/views/menu.js
+++ public/dist/js/spa/views/menu.js
@@ -12,6 +12,19 @@ export default class extends Constructor {
     }
 
     async getHtml() {
-        return `this is menu page`;
+        return `
+            <div id="menu__app">
+                <div class="card">
+                    <div class="card-header">Helps & Settings</div>
+                    <div class="card-body">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action"><i class="bx bx-info-circle menu__icon"></i> Help</a>
+                            <a href="#" class="list-group-item list-group-item-action"><i class="bx bx-cog menu__icon"></i> Settings</a>
+                            <a href="#" class="list-group-item list-group-item-action"><i class="bx bx-log-out menu__icon"></i> Log Out</a>
+                        </div>
+                    <div>
+                </div>
+            </div>
+        `;
     }
 }
