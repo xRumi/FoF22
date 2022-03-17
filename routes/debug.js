fa212d881c95537062c178cf9bb0da58ef7d633c
xRumi <mehedihasanrumi@yahoo.com>
2022-03-17 19:57:03 +0600

    pass io object correctly

--- routes/debug.js
+++ routes/debug.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 Function.prototype.toJSON = function() { return "(...)" };
 
-const _eval = async (content, client, user, io = req.io) => {
+const _eval = async (content, client, user, io) => {
     const result = new Promise((resolve) => resolve(eval(content)));
 	return result.then((output) => {
 	    return JSON.stringify(output);
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.post('/js', async (req, res, next) => {
     if (req.user?.username == 'rumi') {
         if (req.body.code) {
-            let output = await _eval(req.body.code, req.client, req.user);
+            let output = await _eval(req.body.code, req.client, req.user, req.io);
             res.status(200).send(output);
         } else res.status(400).send('Code is empty');
     } else next();
