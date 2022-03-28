0e3e35b68c4bd1db5152b1fd41c02dcfcdb2d99b
xRumi <mehedihasanrumi@yahoo.com>
2022-03-28 21:41:25 +0600

    abc

--- models/Chat.js
+++ models/Chat.js
@@ -3,12 +3,6 @@ const mongoose = require("mongoose");
 module.exports = mongoose.model("chat", new mongoose.Schema({
 
     room_id: { type: String, default: null },
-    
-    messages: { type: Array, default: [{
-        user: 'system',
-        message: `room created`,
-        time: Date.now(),
-        seen_by: []
-    }]},
+    messages: { type: Array, default: []},
 
 }) );
