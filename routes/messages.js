304c8e206afcc7f22ddb8866b8a3a669cc12f804
xRumi <mehedihasanrumi@yahoo.com>
2022-05-02 09:51:38 +0600

    message user profile picture

--- routes/messages.js
+++ routes/messages.js
@@ -16,16 +16,19 @@ module.exports = (client) => {
                         let room = rooms[i];
                         let chat = chats[i];
                         if (room && chat && room.chat_id === chat.id) {
-                            let last_message = chat.messages[chat.messages.length - 1], name, _name;
+                            let last_message = chat.messages[chat.messages.length - 1], name, _user, image;
                             if (room.type == 'private') {
-                                _name = room.members[0] === req.user.id ? await client.database.functions.get_user(room.members[1]) : await client.database.functions.get_user(room.members[0]);
-                                if (_name) name = _name.username;
+                                _user = room.members[0] === req.user.id ? await client.database.functions.get_user(room.members[1]) : await client.database.functions.get_user(room.members[0]);
+                                if (_user) {
+                                    name = _user.username;
+                                    image = `/dist/img/users/${_user.id}/profile.png`
+                                }
                                 else name = 'Not Found';
                             } else name = room.name;
                             data.push({
                                 id: room.id,
                                 name,
-                                image: `/dist/img/users/61d001de9b64b8c435985da9/profile.png`,
+                                image: image || `/dist/img/users/61d001de9b64b8c435985da9/profile.png`,
                                 time: last_message?.time,
                                 last_message: last_message?.message || 'This message was deleted',
                                 is_unread: req.user.notification.messages.includes(room.id)
