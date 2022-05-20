79c4f24e749064c32424678aa889b44ae9a559b0
xRumi <mehedihasanrumi@yahoo.com>
2022-05-20 07:26:09 +0600

    fix

--- routes/messages.js
+++ routes/messages.js
@@ -11,9 +11,11 @@ module.exports = (client) => {
         if (req.user) {
             Promise.all(req.user.rooms.map(x => client.database.functions.get_room(x.id))).then(rooms => {
                 Promise.all(rooms.map(room => client.database.chat.findById(room.chat_id))).then(async chats => {
-                    let data = chats.map(async chat => {
-                        let room = rooms.find(y => y.chat_id == chat.id);
-                        if (room && chat && chat.room_id == chat.id) {
+                    let data = [];
+                    for (var i = 0; i < rooms.length; i++) {
+                        let room = rooms[i];
+                        let chat = chats.find(x => x.room_id == room?.id);
+                        if (room && chat && room.chat_id == chat.id) {
                             let last_message = chat.messages[chat.messages.length - 1], name, _user, image;
                             if (room.type == 'private') {
                                 _user = await client.database.functions.get_user(room.members[0] == req.user.id ? room.members[1] : room.members[0]);
@@ -23,17 +25,16 @@ module.exports = (client) => {
                                 } else name = 'Not Found';
                             } else name = room.name;
                             let user_room_unread = req.user.rooms.find(z => z.id == room.id)?.unread;
-                            return {
+                            data.push({
                                 id: room.id,
                                 name,
                                 image: image || `/dist/img/users/61d001de9b64b8c435985da9/profile.png`,
                                 time: last_message?.time,
                                 last_message: last_message?.message || 'This message was deleted',
                                 unread: user_room_unread
-                            };
+                            });
                         }
-                    });
-                    console.log(data);
+                    }
                     res.status(200).send(data);
                 });
             });
